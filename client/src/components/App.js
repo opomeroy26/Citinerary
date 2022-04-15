413a9cc81d51241615cea68c6777ab2f4d6346b4
Olivia Pomeroy <opomeroy26@gmail.com>
2022-04-15 09:52:41 -0700

    Initial Commit. Setup

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Test Content
+      </header>
+    </div>
+  );
+}
+
+export default App;
