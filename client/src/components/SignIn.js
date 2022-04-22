9a81e81ef850ebb05d60459c2b0fef81c250984b
Hannah Glazier <hrayglazier@aol.com>
2022-04-22 09:17:58 -0600

    background image

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -76,7 +76,7 @@ export default function SignIn( {onSignIn} ) {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
+            backgroundImage: 'url(https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
